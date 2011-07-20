319435127dc737f21881db3cfd5e7c3e5ba3e761
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2011-07-20 13:24:50 -0700

    Add helper class with definition for logger

---
+++ Nina/NinaHelper.h
@@ -0,0 +1,25 @@
+//
+//  NinaHelper.h
+//  Nina
+//
+//  Created by Ian MacKinnon on 11-07-20.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+//single place to define debugging or not -iMack
+//#define DEBUG 
+
+#ifdef DEBUG
+#   define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+#else
+#   define DLog(...)
+#endif
+
+#import <Foundation/Foundation.h>
+
+
+@interface NinaHelper : NSObject {
+    
+}
+
+@end
---
+++ Nina/NinaHelper.m
@@ -0,0 +1,14 @@
+//
+//  NinaHelper.m
+//  Nina
+//
+//  Created by Ian MacKinnon on 11-07-20.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "NinaHelper.h"
+
+
+@implementation NinaHelper
+
+@end
