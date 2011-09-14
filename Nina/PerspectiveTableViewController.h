9feb520829eb46b85c316a9eca0dc8f2b3a7623b
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2011-09-14 15:36:20 -0700

    cleanup some commits from project, mostly deletes and a few adds

---
+++ Nina/PerspectiveTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PerspectiveTableViewController.h
+//  Nina
+//
+//  Created by Ian MacKinnon on 11-08-23.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PerspectiveTableViewController : UITableViewController
+
+@end
---
+++ Nina/PlacesListView.h
@@ -0,0 +1,13 @@
+//
+//  PlacesListView.h
+//  Nina
+//
+//  Created by Ian MacKinnon on 11-09-12.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlacesListView : UITableViewController
+
+@end
