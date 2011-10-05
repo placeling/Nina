261419bd9a49bce4e8d57f0c50ee091f8f23fc8d
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2011-10-05 09:13:22 -0700

    having clicking on a cell popup the page view

--- Nina/NearbySuggestedPlaceController.m
+++ Nina/NearbySuggestedPlaceController.m
@@ -8,6 +8,7 @@
 
 #import "NearbySuggestedPlaceController.h"
 #import "NSString+SBJSON.h"
+#import "PlacePageViewController.h"
 #import "Place.h"
 
 @interface NearbySuggestedPlaceController (Private)
@@ -269,57 +270,22 @@
     return cell;
 }
 
-/*
- // Override to support conditional editing of the table view.
- - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
- {
- // Return NO if you do not want the specified item to be editable.
- return YES;
- }
- */
-
-/*
- // Override to support editing the table view.
- - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
- {
- if (editingStyle == UITableViewCellEditingStyleDelete) {
- // Delete the row from the data source
- [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
- }   
- else if (editingStyle == UITableViewCellEditingStyleInsert) {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }   
- }
- */
-
-/*
- // Override to support rearranging the table view.
- - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
- {
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
- {
- // Return NO if you do not want the item to be re-orderable.
- return YES;
- }
- */
 
 #pragma mark - Table view delegate
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     [detailViewController release];
-     */
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    if (indexPath.row < [nearbyPlaces count]){
+        Place *place = [nearbyPlaces objectAtIndex:indexPath.row];
+        
+        PlacePageViewController *placeController = [[PlacePageViewController alloc] initWithPlace:place];
+        [self.navigationController pushViewController:placeController animated:TRUE];
+        [placeController release];
+        
+    }
+    
+    
 }
 
 
