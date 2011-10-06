f900b15770178e66bce0c85dde0bd46c35bff0b9
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2011-10-05 17:54:03 -0700

    Add a controller for editing profile of a user

---
+++ Nina/EditProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditProfileViewController.h
+//  Nina
+//
+//  Created by Ian MacKinnon on 11-10-05.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditProfileViewController : UITableViewController
+
+@end
