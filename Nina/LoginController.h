ff98d5badf3b5633e47bbfb0fc786a7be03384af
Lindsay Watt <lindsayrgwatt@gmail.com>
2011-11-01 13:08:59 -0700

    Updated login page

--- Nina/LoginController.h
+++ Nina/LoginController.h
@@ -14,21 +14,24 @@
 
 @protocol LoginControllerDelegate;
 
-@interface LoginController : UIViewController<UITextFieldDelegate, ASIHTTPRequestDelegate, UITextFieldDelegate, FBRequestDelegate>{
+@interface LoginController : UIViewController<UITextFieldDelegate, ASIHTTPRequestDelegate, UITextFieldDelegate, FBRequestDelegate, UIGestureRecognizerDelegate>{
     IBOutlet UITextField *username;
     IBOutlet UITextField *password;
     IBOutlet UIButton *submitButton;
+    IBOutlet UIButton *forgotPasswordButton;
     id <LoginControllerDelegate> delegate;
 }
 
 @property(nonatomic, retain) IBOutlet UITextField *username;
 @property(nonatomic, retain) IBOutlet UITextField *password;
 @property(nonatomic, retain) IBOutlet UIButton *submitButton;
+@property(nonatomic, retain) IBOutlet UIButton *forgotPasswordButton;
 @property(nonatomic, assign) id <LoginControllerDelegate> delegate;
 
 -(IBAction)cancel;
 
 -(IBAction) submitLogin;
+-(IBAction) forgotPassword;
 -(IBAction) signupFacebook;
 -(IBAction) signupOldSchool;
 
