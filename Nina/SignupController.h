f2f8b5bcad025149f6be6312c2972d3898bf484f
Lindsay Watt <lindsayrgwatt@gmail.com>
2011-11-01 15:21:08 -0700

    Updated registration page

--- Nina/SignupController.h
+++ Nina/SignupController.h
@@ -11,18 +11,28 @@
 #import "ASIHTTPRequest.h"
 #import "Facebook.h"
 
-@interface SignupController : UITableViewController<ASIHTTPRequestDelegate>{
+@interface SignupController : UITableViewController<ASIHTTPRequestDelegate, UITextFieldDelegate>{
     NSDictionary *fbDict;
     
     NSString *accessKey;
     NSString *accessSecret;
+    
+    IBOutlet UIView *tableFooterView;
+    IBOutlet UIButton *termsButton;
+    IBOutlet UIButton *privacyButton;
+
 }
 
 
 @property(nonatomic,retain) NSDictionary *fbDict;
 @property(nonatomic,retain) NSString *accessKey;
 @property(nonatomic,retain) NSString *accessSecret;
+@property(nonatomic,retain) IBOutlet UIView *tableFooterView;
+@property(nonatomic,retain) IBOutlet UIButton *termsButton;
+@property(nonatomic,retain) IBOutlet UIButton *privacyButton;
 
 -(IBAction)signup;
+-(IBAction)showTerms;
+-(IBAction)showPrivacy;
 
 @end
