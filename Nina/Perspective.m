b298727c750d7e8d7415c9f5a8a2f6f416e83cb4
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2011-12-10 13:12:10 -0800

    NIN-148, just reset photos when get update from server

--- Nina/Perspective.m
+++ Nina/Perspective.m
@@ -38,23 +38,13 @@
 -(void) updateFromJsonDict:(NSDictionary *)jsonDict{
     mine = [[jsonDict objectForKey:@"mine"] boolValue];
     
+    [self.photos removeAllObjects];
     for (NSDictionary *photoDict in [jsonDict objectForKey:@"photos"]){
-        bool found = false;
-        for (Photo *photo in self.photos){
-            if ([[photoDict objectForKey:@"_id"] isEqualToString:photo.photo_id]){
-                found = true;
-                photo.mine = mine;
-                break;
-            }
-        }
-        
-        if (!found){
-            Photo *photo = [[Photo alloc] initFromJsonDict:photoDict];
-            photo.perspective = self;
-            photo.mine = self.mine;
-            [self.photos addObject:photo];
-            [photo release];
-        }
+        Photo *photo = [[Photo alloc] initFromJsonDict:photoDict];
+        photo.perspective = self;
+        photo.mine = self.mine;
+        [self.photos addObject:photo];
+        [photo release];
     }    
     
     self.perspectiveId = [jsonDict objectForKeyNotNull:@"_id"];
