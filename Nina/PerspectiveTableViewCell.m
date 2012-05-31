10d50942f0c1529152c2f5630a2a7585f1746707
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-05-30 21:40:29 -0700

    have a newline

--- Nina/PerspectiveTableViewCell.m
+++ Nina/PerspectiveTableViewCell.m
@@ -290,7 +290,7 @@
     
     if ( !currentUser ) {
         UIAlertView *baseAlert;
-        NSString *alertMessage = @"Sign up or log in to share or flag this placemark";
+        NSString *alertMessage = @"Sign up or log in to share\n or flag this placemark";
         baseAlert = [[UIAlertView alloc] 
                      initWithTitle:nil message:alertMessage 
                      delegate:self.requestDelegate cancelButtonTitle:@"Not Now" 
