07b5925c2553ba4cd9d17c5298a5ce517fddb5cd
imackinn <imackinn@alumni.uwaterloo.ca>
2012-07-12 11:57:02 -0700

    NIN-402, don't automatically unhilight a post when notification opened

--- Nina/NinaAppDelegate.m
+++ Nina/NinaAppDelegate.m
@@ -156,7 +156,7 @@
             [self.navigationController pushViewController:nearbySuggestedMapController animated:false];
             [nearbySuggestedMapController release];
             
-            NSString *urlText = [NSString stringWithFormat:@"/v1/places/%@/unhighlight", placeId]; //unhilight, so we don't send again
+            //NSString *urlText = [NSString stringWithFormat:@"/v1/places/%@/unhighlight", placeId]; //unhilight, so we don't send again
             
             [[RKClient sharedClient] post:urlText params:nil delegate:nil]; 
         } else if ( [[url host] isEqualToString:@"facebookfriends"] ){
