50dd930512a91d867c258605c8eec8b006cea531
imackinn <imackinn@alumni.uwaterloo.ca>
2012-08-28 11:32:17 -0700

    NIN-103, have a standard background colour keyboard

--- Nina/lib/YIPopupTextView.m
+++ Nina/lib/YIPopupTextView.m
@@ -148,9 +148,10 @@ typedef enum {
         self.frame = UIEdgeInsetsInsetRect(_popupView.frame, TEXTVIEW_INSETS);
         self.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
         self.font = [UIFont systemFontOfSize:TEXT_SIZE];
-        self.keyboardAppearance = UIKeyboardAppearanceAlert;
+        //self.keyboardAppearance = UIKeyboardAppearanceAlert;
         self.autocorrectionType = UITextAutocorrectionTypeNo;
         self.autocapitalizationType = UITextAutocapitalizationTypeNone;
+        
         self.layer.cornerRadius = 10;
         self.backgroundColor = [UIColor whiteColor];
         [_popupView addSubview:self];
