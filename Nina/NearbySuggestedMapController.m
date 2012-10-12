ad1b619cdf8b6ad0e6c95afeeb4b5904cc54a373
imackinn <imackinn@alumni.uwaterloo.ca>
2012-10-12 12:37:57 -0700

    NIN-483, put the scroll threshold for update to a quarter from a half

--- Nina/NearbySuggestedMapController.m
+++ Nina/NearbySuggestedMapController.m
@@ -299,7 +299,7 @@
     CLLocationDegrees mLng = center.longitude;
     
     //without dataLoaded, the first time this runs it will get a whole world map
-    if ( (fabs(uLat - mLat) > region.span.latitudeDelta/2 || fabs(uLng - mLng) > region.span.longitudeDelta/2 || region.span.latitudeDelta > 2.5*lastLatSpan || region.span.latitudeDelta < lastLatSpan/2.5) ){
+    if ( (fabs(uLat - mLat) > region.span.latitudeDelta/4 || fabs(uLng - mLng) > region.span.longitudeDelta/4 || region.span.latitudeDelta > 2.5*lastLatSpan || region.span.latitudeDelta < lastLatSpan/2.5) ){
         
         if ( viewLoaded ){        
 
